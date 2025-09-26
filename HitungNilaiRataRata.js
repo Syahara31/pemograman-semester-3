a5d6503a59abc3832b770cb86f70cff716f5ec24
Syahara31 <syahara.nazwa_ti24@nusaputra.ac.id>
2025-09-26 10:39:40 +0700

    Add files via upload

---
+++ HitungNilaiRataRata.js
@@ -0,0 +1,20 @@
+// Deklarasi Variabel dengan masing - masing nilai
+let matematika = 85;
+let BahasaInggris = 90;
+let Ipa = 85;
+
+// Perhitungan Rata -Rata
+let NilaiRataRata = (matematika + BahasaInggris + Ipa) / 3;
+
+// Tipe data dan output
+console.log("Nilai matematika :" + matematika);
+console.log("Nilai Bahasa inggris :" + BahasaInggris);
+console.log("Nilai Ipa :" + Ipa);
+console.log("Nilai Rata Rata :" + NilaiRataRata);
+
+// Struktur Kondisi (if-else)
+if (NilaiRataRata >=80) {
+    console.log("Selamat Anda Lulus!")
+} else {
+    console.log("Maaf anda tidak Lulus")
+}
